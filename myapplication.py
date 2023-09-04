8a6d447e93c88d7e7db55477cf94dca16239290c
pushpak838 <64265579+pushpak838@users.noreply.github.com>
2023-09-04 11:56:44 +0000

    Create myapplication.py

---
+++ myapplication.py
@@ -0,0 +1,7 @@
+def main():
+    # Print "Hello, World!" to the console
+    print("Hello, World!")
+
+if __name__ == "__main__":
+    # This block of code will only be executed if this script is run directly (not imported as a module)
+    main()
